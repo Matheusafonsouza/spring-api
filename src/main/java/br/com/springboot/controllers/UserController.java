e90fa4542265b6d9082d54793859311b819b3cff
matheusafonsouza <matheusafonsouza@gmail.com>
2022-02-14 12:55:53 -0300

    using path id for get route

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -2,8 +2,10 @@ package br.com.springboot.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +19,14 @@ public class UserController {
 
   private List<User> users = new ArrayList<>();
 
-  @GetMapping("/")
-  public User user() {
-    User user = new User();
-    user.setId(1L);
-    user.setName("Matheus");
-    user.setUsername("afonso");
-    return user;
+  @GetMapping("/{id}")
+  public User user(@PathVariable("id") Long id) {
+    Optional<User> userFind = users.stream().filter(user -> user.getId() == id).findFirst();
+
+    if (userFind.isPresent()) {
+      return userFind.get();
+    }
+    return null;
   }
 
   @PostMapping("/")
